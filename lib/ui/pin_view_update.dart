9453a99e5f9986ee106b93e2a5bca433bf271247
abhijeet <asingh@mind-roots.com>
2019-07-10 11:28:03 +0530

    otpresend both

--- lib/ui/pin_view_update.dart
+++ lib/ui/pin_view_update.dart
@@ -106,6 +106,7 @@ class PinState extends State<PinViewUpdatePage> {
                   onTap: () {
                     var bloc = UpdatePhoneBloc();
                     bloc.fetchData(widget.phone);
+
                   },
                   child: Center(
                     child: Text(
